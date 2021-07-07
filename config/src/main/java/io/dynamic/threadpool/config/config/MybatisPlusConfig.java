9dcd5fb4c75ea5e4a4b2368a5cb270943722a736
chen.ma <machencoding@163.com>
2021-07-07 18:47:15 +0800

    fix: 修复 Mybatis Plus 分页组件数据库.

--- config/src/main/java/io/dynamic/threadpool/config/config/MybatisPlusConfig.java
+++ config/src/main/java/io/dynamic/threadpool/config/config/MybatisPlusConfig.java
@@ -18,7 +18,7 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
 
