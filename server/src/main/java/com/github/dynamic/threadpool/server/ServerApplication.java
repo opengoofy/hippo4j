c7c4a21abfc285925c9d24704545477b530c5ca3
chen.ma <machencoding@163.com>
2021-10-30 22:38:32 +0800

    feature: 添加用户权限 Mapper 扫描包目录.

--- server/src/main/java/com/github/dynamic/threadpool/server/ServerApplication.java
+++ server/src/main/java/com/github/dynamic/threadpool/server/ServerApplication.java
@@ -4,8 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.github.dynamic.threadpool.config.mapper")
-@SpringBootApplication(scanBasePackages = "com.github.dynamic.threadpool")
+@MapperScan(basePackages =
+        {
+                "com.github.dynamic.threadpool.config.mapper",
+                "com.github.dynamic.threadpool.auth.mapper"
+        })
+@SpringBootApplication(scanBasePackages =
+        {
+                "com.github.dynamic.threadpool"
+        })
 public class ServerApplication {
 
     public static void main(String[] args) {
