c4f9a31806203cd0c8f9d4c939d967011d5cdd5f
chen.ma <machencoding@163.com>
2021-11-16 20:15:51 +0800

    Hippo4J 移除前端模块, 独立为新的项目.

--- hippo4j-console-ui/plopfile.js
+++
@@ -1,7 +0,0 @@
-const viewGenerator = require('./plop-templates/view/prompt')
-const componentGenerator = require('./plop-templates/component/prompt')
-
-module.exports = function(plop) {
-  plop.setGenerator('view', viewGenerator)
-  plop.setGenerator('component', componentGenerator)
-}
