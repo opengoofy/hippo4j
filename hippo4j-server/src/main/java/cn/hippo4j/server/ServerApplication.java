f4a8f289978a9caa95c2ad6f985eeb9299043eea
chen.ma <machencoding@163.com>
2021-11-17 21:57:54 +0800

    优化 ServerApplication 代码配置样式.

--- hippo4j-server/src/main/java/cn/hippo4j/server/ServerApplication.java
+++ hippo4j-server/src/main/java/cn/hippo4j/server/ServerApplication.java
@@ -4,15 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages =
-        {
-                "cn.hippo4j.config.mapper",
-                "cn.hippo4j.auth.mapper"
-        })
-@SpringBootApplication(scanBasePackages =
-        {
-                "cn.hippo4j"
-        })
+@SpringBootApplication(scanBasePackages = "cn.hippo4j")
+@MapperScan(basePackages = {"cn.hippo4j.config.mapper", "cn.hippo4j.auth.mapper"})
 public class ServerApplication {
 
     public static void main(String[] args) {
