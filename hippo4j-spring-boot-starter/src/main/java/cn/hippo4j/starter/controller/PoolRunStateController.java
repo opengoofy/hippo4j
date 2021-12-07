f3f2c9378dc6b4d52fe699ae14c10065c73d1da4
chen.ma <machencoding@163.com>
2021-12-07 21:55:03 +0800

    修改获取动态线程池运行状态接口.

--- hippo4j-spring-boot-starter/src/main/java/cn/hippo4j/starter/controller/PoolRunStateController.java
+++ hippo4j-spring-boot-starter/src/main/java/cn/hippo4j/starter/controller/PoolRunStateController.java
@@ -4,6 +4,7 @@ import cn.hippo4j.common.model.PoolRunStateInfo;
 import cn.hippo4j.common.web.base.Result;
 import cn.hippo4j.common.web.base.Results;
 import cn.hippo4j.starter.handler.ThreadPoolRunStateHandler;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @CrossOrigin
 @RestController
+@AllArgsConstructor
 public class PoolRunStateController {
+
+    private final ThreadPoolRunStateHandler threadPoolRunStateHandler;
 
     @GetMapping("/run/state/{tpId}")
     public Result<PoolRunStateInfo> getPoolRunState(@PathVariable("tpId") String tpId) {
-        PoolRunStateInfo poolRunState = ThreadPoolRunStateHandler.getPoolRunState(tpId);
+        PoolRunStateInfo poolRunState = threadPoolRunStateHandler.getPoolRunState(tpId);
         return Results.success(poolRunState);
     }
 
