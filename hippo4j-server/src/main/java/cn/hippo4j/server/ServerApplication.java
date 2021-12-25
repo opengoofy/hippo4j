b77cfc0e9c48c0ebd87716b314baeb0440cd40d5
chen.ma <machencoding@163.com>
2021-12-25 19:35:20 +0800

    开启 spring 全局事务管理器.

--- hippo4j-server/src/main/java/cn/hippo4j/server/ServerApplication.java
+++ hippo4j-server/src/main/java/cn/hippo4j/server/ServerApplication.java
@@ -3,7 +3,9 @@ package cn.hippo4j.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication(scanBasePackages = "cn.hippo4j")
 @MapperScan(basePackages = {"cn.hippo4j.config.mapper", "cn.hippo4j.auth.mapper"})
 public class ServerApplication {
