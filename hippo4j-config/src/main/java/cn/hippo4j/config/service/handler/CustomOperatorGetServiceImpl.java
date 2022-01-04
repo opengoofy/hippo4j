6123ca3f3ca7c8325f200583c42ffcaa1e769b38
chen.ma <machencoding@163.com>
2022-01-05 00:15:33 +0800

    修改默认获取操作人日志组件.

--- hippo4j-config/src/main/java/cn/hippo4j/config/service/handler/CustomOperatorGetServiceImpl.java
+++ hippo4j-config/src/main/java/cn/hippo4j/config/service/handler/CustomOperatorGetServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.hippo4j.config.service.handler;
 
+import cn.hippo4j.common.toolkit.StringUtil;
 import cn.hippo4j.common.toolkit.UserContext;
 import cn.hippo4j.tools.logrecord.model.Operator;
 import cn.hippo4j.tools.logrecord.service.OperatorGetService;
@@ -16,7 +17,9 @@ public class CustomOperatorGetServiceImpl implements OperatorGetService {
 
     @Override
     public Operator getUser() {
-        return new Operator(UserContext.getUserName());
+        String userName = UserContext.getUserName();
+        userName = StringUtil.isEmpty(userName) ? "-" : userName;
+        return new Operator(userName);
     }
 
 }
