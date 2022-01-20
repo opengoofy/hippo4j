3d7792500a272d5dd3ecdc1f448e0c08437c7f35
chen.ma <machencoding@163.com>
2022-01-20 22:22:40 +0800

    变更监听 Spring 事件.

--- hippo4j-spring-boot-starter/src/main/java/cn/hippo4j/starter/event/ApplicationContentPostProcessor.java
+++ hippo4j-spring-boot-starter/src/main/java/cn/hippo4j/starter/event/ApplicationContentPostProcessor.java
@@ -1,8 +1,8 @@
 package cn.hippo4j.starter.event;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 
 import javax.annotation.Resource;
 
@@ -12,7 +12,7 @@ import javax.annotation.Resource;
  * @author chen.ma
  * @date 2021/12/25 20:21
  */
-public class ApplicationContentPostProcessor implements ApplicationListener<ContextRefreshedEvent> {
+public class ApplicationContentPostProcessor implements ApplicationListener<ApplicationReadyEvent> {
 
     @Resource
     private ApplicationContext applicationContext;
@@ -20,7 +20,7 @@ public class ApplicationContentPostProcessor implements ApplicationListener<Cont
     private boolean executeOnlyOnce = true;
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void onApplicationEvent(ApplicationReadyEvent event) {
         synchronized (ApplicationContentPostProcessor.class) {
             if (executeOnlyOnce) {
                 applicationContext.publishEvent(new ApplicationCompleteEvent(this));
