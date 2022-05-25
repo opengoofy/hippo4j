696fb2ce12225fa4d868d0388955ed27daeb3a1e
chen.ma <machencoding@163.com>
2022-05-25 22:42:46 +0800

    Rabbitmq thread pool config code formatting

--- hippo4j-example/hippo4j-spring-boot-starter-adapter-rabbitmq-example/src/main/java/cn/hippo4j/springboot/starter/adapter/rabbitmq/example/config/RabbitMQThreadPoolConfig.java
+++ hippo4j-example/hippo4j-spring-boot-starter-adapter-rabbitmq-example/src/main/java/cn/hippo4j/springboot/starter/adapter/rabbitmq/example/config/RabbitMQThreadPoolConfig.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.hippo4j.springboot.starter.adapter.rabbitmq.example.config;
 
 import org.springframework.amqp.rabbit.config.AbstractRabbitListenerContainerFactory;
@@ -16,22 +33,23 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class RabbitMQThreadPoolConfig {
 
-    
     @Bean
     public ThreadPoolTaskExecutor rabbitListenerTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setMaxPoolSize(10); // 指定线程的最大数量
-        executor.setCorePoolSize(2); // 指定线程池维护线程的最少数量
-        executor.setQueueCapacity(20); // 指定等待处理的任务数
+        // 指定线程的最大数量
+        executor.setMaxPoolSize(10);
+        // 指定线程池维护线程的最少数量
+        executor.setCorePoolSize(2);
+        // 指定等待处理的任务数
+        executor.setQueueCapacity(20);
         executor.setThreadNamePrefix("RabbitListenerTaskExecutor-");
         return executor;
     }
 
     @Bean
-    public AbstractRabbitListenerContainerFactory<?> defaultRabbitListenerContainerFactory (
-            SimpleRabbitListenerContainerFactoryConfigurer configurer,
-            ThreadPoolTaskExecutor rabbitListenerTaskExecutor,
-            ConnectionFactory connectionFactory) {
+    public AbstractRabbitListenerContainerFactory<?> defaultRabbitListenerContainerFactory(SimpleRabbitListenerContainerFactoryConfigurer configurer,
+                                                                                           ThreadPoolTaskExecutor rabbitListenerTaskExecutor,
+                                                                                           ConnectionFactory connectionFactory) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         configurer.configure(factory, connectionFactory);
         factory.setConcurrentConsumers(1);
@@ -39,6 +57,4 @@ public class RabbitMQThreadPoolConfig {
         factory.setTaskExecutor(rabbitListenerTaskExecutor);
         return factory;
     }
-
-
 }
