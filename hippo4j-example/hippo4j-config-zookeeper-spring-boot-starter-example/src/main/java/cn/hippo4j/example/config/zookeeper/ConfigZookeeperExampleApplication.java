0d9ef6e186264e8b2183e3c6eb3badeb0e9e4db9
lijianxin <1064730540@qq.com>
2022-09-01 20:58:24 +0800

    feat: hippo4j-config-example rename core to config

--- hippo4j-example/hippo4j-config-zookeeper-spring-boot-starter-example/src/main/java/cn/hippo4j/example/config/zookeeper/ConfigZookeeperExampleApplication.java
+++ hippo4j-example/hippo4j-config-zookeeper-spring-boot-starter-example/src/main/java/cn/hippo4j/example/config/zookeeper/ConfigZookeeperExampleApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableDynamicThreadPool
-@SpringBootApplication(scanBasePackages = "cn.hippo4j.example.core")
+@SpringBootApplication(scanBasePackages = "cn.hippo4j.example.config")
 public class ConfigZookeeperExampleApplication {
 
     public static void main(String[] args) {
