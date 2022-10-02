ee949adcfc338e7433fe18a5da474af0b6e21c2f
guoz <763366136@qq.com>
2022-10-02 20:23:59 +0800

    add Md5ConfigUtilTest test case

--- hippo4j-adapter/hippo4j-adapter-base/src/main/java/cn/hippo4j/adapter/base/ThreadPoolAdapterBeanContainer.java
+++ hippo4j-adapter/hippo4j-adapter-base/src/main/java/cn/hippo4j/adapter/base/ThreadPoolAdapterBeanContainer.java
@@ -17,7 +17,6 @@
 
 package cn.hippo4j.adapter.base;
 
-import cn.hippo4j.common.config.ApplicationContextHolder;
 import com.google.common.collect.Maps;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -35,7 +34,8 @@ public class ThreadPoolAdapterBeanContainer implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        Map<String, ThreadPoolAdapter> threadPoolAdapterMap = ApplicationContextHolder.getBeansOfType(ThreadPoolAdapter.class);
-        threadPoolAdapterMap.forEach((key, val) -> THREAD_POOL_ADAPTER_BEAN_CONTAINER.put(val.mark(), val));
-    }
+        /*
+         * Map<String, ThreadPoolAdapter> threadPoolAdapterMap = ApplicationContextHolder.getBeansOfType(ThreadPoolAdapter.class); threadPoolAdapterMap.forEach((key, val) ->
+         * THREAD_POOL_ADAPTER_BEAN_CONTAINER.put(val.mark(), val));
+         */}
 }
