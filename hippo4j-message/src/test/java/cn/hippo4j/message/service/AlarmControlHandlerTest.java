4185932655fc4c1413d2dbf8d15cbe3cc96a47d7
Lijx <lijinxingerm@gmail.com>
2022-10-09 23:42:55 +0800

    AlarmControlHandler supplement test cases

--- hippo4j-message/src/test/java/cn/hippo4j/message/service/AlarmControlHandlerTest.java
+++ hippo4j-message/src/test/java/cn/hippo4j/message/service/AlarmControlHandlerTest.java
@@ -38,14 +38,4 @@ public class AlarmControlHandlerTest {
         alarmControlHandler.initCacheAndLock("1", "Wechat", 1);
         Assert.assertTrue(alarmControlHandler.isSendAlarm(alarmControlDTO));
     }
-
-    @Test
-    public void assertExpireAfterSendAlarm() throws InterruptedException {
-        AlarmControlHandler alarmControlHandler = new AlarmControlHandler();
-        AlarmControlDTO alarmControlDTO = new AlarmControlDTO("1", "Wechat", NotifyTypeEnum.ACTIVITY);
-        alarmControlHandler.initCacheAndLock("1", "Wechat", 1);
-        alarmControlHandler.isSendAlarm(alarmControlDTO);
-        Thread.sleep(60 * 1000);
-        Assert.assertTrue(alarmControlHandler.isSendAlarm(alarmControlDTO));
-    }
 }
