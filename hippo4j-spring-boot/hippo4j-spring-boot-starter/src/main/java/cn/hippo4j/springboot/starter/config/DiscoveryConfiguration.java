c423ffa4483f89d381fe1764fa43ed31494643e3
马称 Ma Chen <machen@apache.org>
2022-11-01 23:18:31 +0800

    Client instance registration information code refactoring (#888) (#889)

--- hippo4j-spring-boot/hippo4j-spring-boot-starter/src/main/java/cn/hippo4j/springboot/starter/config/DiscoveryConfiguration.java
+++ hippo4j-spring-boot/hippo4j-spring-boot-starter/src/main/java/cn/hippo4j/springboot/starter/config/DiscoveryConfiguration.java
@@ -17,25 +17,14 @@
 
 package cn.hippo4j.springboot.starter.config;
 
-import cn.hippo4j.common.api.ClientNetworkService;
 import cn.hippo4j.common.model.InstanceInfo;
-import cn.hippo4j.common.spi.DynamicThreadPoolServiceLoader;
-import cn.hippo4j.common.toolkit.ContentUtil;
-import cn.hippo4j.core.toolkit.IdentifyUtil;
 import cn.hippo4j.core.toolkit.inet.InetUtils;
 import cn.hippo4j.springboot.starter.core.DiscoveryClient;
+import cn.hippo4j.springboot.starter.provider.InstanceInfoProviderFactory;
 import cn.hippo4j.springboot.starter.remote.HttpAgent;
-import cn.hippo4j.springboot.starter.toolkit.CloudCommonIdUtil;
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
-
-import java.net.InetAddress;
-import java.util.Optional;
-
-import static cn.hippo4j.common.constant.Constants.IDENTIFY_SLICER_SYMBOL;
-import static cn.hippo4j.core.toolkit.IdentifyUtil.CLIENT_IDENTIFICATION_VALUE;
 
 /**
  * Dynamic thread-pool discovery config.
@@ -49,43 +38,9 @@ public class DiscoveryConfiguration {
 
     private final InetUtils hippo4JInetUtils;
 
-    static {
-        DynamicThreadPoolServiceLoader.register(ClientNetworkService.class);
-    }
-
     @Bean
-    @SneakyThrows
     public InstanceInfo instanceConfig() {
-        String namespace = bootstrapProperties.getNamespace();
-        String itemId = bootstrapProperties.getItemId();
-        String port = environment.getProperty("server.port", "8080");
-        String applicationName = environment.getProperty("spring.dynamic.thread-pool.item-id");
-        String active = environment.getProperty("spring.profiles.active", "UNKNOWN");
-        InstanceInfo instanceInfo = new InstanceInfo();
-        String instanceId = CloudCommonIdUtil.getDefaultInstanceId(environment, hippo4JInetUtils);
-        instanceId = new StringBuilder()
-                .append(instanceId)
-                .append(IDENTIFY_SLICER_SYMBOL)
-                .append(CLIENT_IDENTIFICATION_VALUE)
-                .toString();
-        String contextPath = environment.getProperty("server.servlet.context-path", "");
-        instanceInfo.setInstanceId(instanceId)
-                .setIpApplicationName(CloudCommonIdUtil.getIpApplicationName(environment, hippo4JInetUtils))
-                .setHostName(InetAddress.getLocalHost().getHostAddress())
-                .setAppName(applicationName)
-                .setPort(port)
-                .setClientBasePath(contextPath)
-                .setGroupKey(ContentUtil.getGroupKey(itemId, namespace));
-        String[] customerNetwork = DynamicThreadPoolServiceLoader.getSingletonServiceInstances(ClientNetworkService.class)
-                .stream().findFirst().map(each -> each.getNetworkIpPort(environment)).orElse(null);
-        String callBackUrl = new StringBuilder().append(Optional.ofNullable(customerNetwork).map(each -> each[0]).orElse(instanceInfo.getHostName())).append(":")
-                .append(Optional.ofNullable(customerNetwork).map(each -> each[1]).orElse(port)).append(instanceInfo.getClientBasePath())
-                .toString();
-        instanceInfo.setCallBackUrl(callBackUrl);
-        String identify = IdentifyUtil.generate(environment, hippo4JInetUtils);
-        instanceInfo.setIdentify(identify);
-        instanceInfo.setActive(active.toUpperCase());
-        return instanceInfo;
+        return InstanceInfoProviderFactory.getInstance(environment, bootstrapProperties, hippo4JInetUtils);
     }
 
     @Bean
