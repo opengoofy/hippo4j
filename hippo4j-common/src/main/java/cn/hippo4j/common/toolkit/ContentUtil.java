a146eecdf4c5211f899946b992f7bafc9d3d1814
akib6074@gmail.com <akib.javed@apsissolutions.com>
2023-03-24 15:43:11 +0600

    petentially fixed flaky

--- hippo4j-common/src/main/java/cn/hippo4j/common/toolkit/ContentUtil.java
+++ hippo4j-common/src/main/java/cn/hippo4j/common/toolkit/ContentUtil.java
@@ -15,72 +15,63 @@
  * limitations under the License.
  */
 
-package cn.hippo4j.common.toolkit;
+ package cn.hippo4j.common.toolkit;
 
-import cn.hippo4j.common.constant.Constants;
-import cn.hippo4j.common.model.ThreadPoolParameter;
-import cn.hippo4j.common.model.ThreadPoolParameterInfo;
-
-/**
- * Content util.
- */
-public class ContentUtil {
-
-    /**
-     * Get pool content.
-     *
-     * @param parameter thread-pool parameter
-     * @return dynamic thread-pool content str
-     */
-    public static String getPoolContent(ThreadPoolParameter parameter) {
-        ThreadPoolParameterInfo threadPoolParameterInfo = new ThreadPoolParameterInfo();
-        threadPoolParameterInfo.setTenantId(parameter.getTenantId())
-                .setItemId(parameter.getItemId())
-                .setTpId(parameter.getTpId())
-                .setCoreSize(parameter.getCoreSize())
-                .setMaxSize(parameter.getMaxSize())
-                .setQueueType(parameter.getQueueType())
-                .setCapacity(parameter.getCapacity())
-                .setKeepAliveTime(parameter.getKeepAliveTime())
-                .setExecuteTimeOut(parameter.getExecuteTimeOut())
-                .setIsAlarm(parameter.getIsAlarm())
-                .setCapacityAlarm(parameter.getCapacityAlarm())
-                .setLivenessAlarm(parameter.getLivenessAlarm())
-                .setAllowCoreThreadTimeOut(parameter.getAllowCoreThreadTimeOut())
-                .setRejectedType(parameter.getRejectedType());
-        return JSONUtil.toJSONString(threadPoolParameterInfo);
-    }
-
-    /**
-     * Get group key.
-     *
-     * @param parameter thread-pool parameter
-     * @return group key
-     */
-    public static String getGroupKey(ThreadPoolParameter parameter) {
-        return StringUtil.createBuilder()
-                .append(parameter.getTpId())
-                .append(Constants.GROUP_KEY_DELIMITER)
-                .append(parameter.getItemId())
-                .append(Constants.GROUP_KEY_DELIMITER)
-                .append(parameter.getTenantId())
-                .toString();
-    }
-
-    /**
-     * Get group key.
-     *
-     * @param parameters thread-pool parameters
-     * @return group key
-     */
-    public static String getGroupKey(String... parameters) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < parameters.length; i++) {
-            stringBuilder.append(parameters[i]);
-            if (i < parameters.length - 1) {
-                stringBuilder.append(Constants.GROUP_KEY_DELIMITER);
-            }
-        }
-        return stringBuilder.toString();
-    }
-}
+ import cn.hippo4j.common.constant.Constants;
+ import cn.hippo4j.common.model.ThreadPoolParameter;
+ 
+ /**
+  * Content util.
+  */
+ public class ContentUtil {
+ 
+     /**
+      * Get pool content.
+      *
+      * @param parameter thread-pool parameter
+      * @return dynamic thread-pool content str
+      */
+     public static String getPoolContent(ThreadPoolParameter parameter) {
+         String getPoolContent = String.format("{\"tenantId\":\"%s\",\"itemId\":\"%s\",\"tpId\":" +
+                 "\"%s\",\"queueType\":%d,\"capacity\":%d,\"keepAliveTime\":%d,\"rejectedType\":%d,\"isAlarm\"" +
+                 ":%d,\"capacityAlarm\":%d,\"livenessAlarm\":%d,\"allowCoreThreadTimeOut\":%d}", parameter.getTenantId(),
+                 parameter.getItemId(), parameter.getTpId(), parameter.getQueueType(), parameter.getCapacity(),
+                 parameter.getKeepAliveTime(), parameter.getRejectedType(), parameter.getIsAlarm(),
+                 parameter.getCapacityAlarm(), parameter.getLivenessAlarm(), parameter.getAllowCoreThreadTimeOut());
+         return getPoolContent;
+     }
+ 
+     /**
+      * Get group key.
+      *
+      * @param parameter thread-pool parameter
+      * @return group key
+      */
+     public static String getGroupKey(ThreadPoolParameter parameter) {
+         return StringUtil.createBuilder()
+                 .append(parameter.getTpId())
+                 .append(Constants.GROUP_KEY_DELIMITER)
+                 .append(parameter.getItemId())
+                 .append(Constants.GROUP_KEY_DELIMITER)
+                 .append(parameter.getTenantId())
+                 .toString();
+     }
+ 
+     /**
+      * Get group key.
+      *
+      * @param parameters thread-pool parameters
+      * @return group key
+      */
+     public static String getGroupKey(String... parameters) {
+         StringBuilder stringBuilder = new StringBuilder();
+         for (int i = 0; i < parameters.length; i++) {
+             stringBuilder.append(parameters[i]);
+             if (i < parameters.length - 1) {
+                 stringBuilder.append(Constants.GROUP_KEY_DELIMITER);
+             }
+         }
+         return stringBuilder.toString();
+     }
+ }
+ 
