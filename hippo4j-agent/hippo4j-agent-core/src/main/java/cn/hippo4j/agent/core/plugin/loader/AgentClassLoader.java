c1bea7a6bf63be9f2ab6f778b37bea491194e878
yanrongzhen <bruceyan@didiglobal.com>
2023-04-30 09:57:12 +0800

    Complete the configuration analysis of SpringBoot 1.x and 2.x versions with spring-boot agent plugins. (#1207)
    * Remove dependency-reduced-pom.xml from the agent core module.
    
    * Complete the configuration analysis of SpringBoot 1.x and 2.x versions through spring-boot agent plugins.

--- hippo4j-agent/hippo4j-agent-core/src/main/java/cn/hippo4j/agent/core/plugin/loader/AgentClassLoader.java
+++ hippo4j-agent/hippo4j-agent-core/src/main/java/cn/hippo4j/agent/core/plugin/loader/AgentClassLoader.java
@@ -17,9 +17,7 @@
 
 package cn.hippo4j.agent.core.plugin.loader;
 
-import cn.hippo4j.agent.core.boot.AgentPackageNotFoundException;
-import cn.hippo4j.agent.core.boot.AgentPackagePath;
-import cn.hippo4j.agent.core.boot.PluginConfig;
+import cn.hippo4j.agent.core.boot.*;
 import cn.hippo4j.agent.core.conf.Config;
 import cn.hippo4j.agent.core.conf.SnifferConfigInitializer;
 import cn.hippo4j.agent.core.logging.api.ILog;
@@ -169,6 +167,13 @@ public class AgentClassLoader extends ClassLoader {
             SnifferConfigInitializer.initializeConfig(pluginConfig.root());
         }
 
+        final SpringBootConfig springBootConfig = loadedClass.getAnnotation(SpringBootConfig.class);
+        if (springBootConfig != null) {
+            // Set up the plugin config when loaded by spring environment is prepared, just scan in here.
+            // Agent class loader just loaded limited classes in the plugin jar(s), so the cost of this
+            // isAssignableFrom would be also very limited.
+            SpringBootConfigInitializer.initializeConfig(springBootConfig);
+        }
         return loadedClass;
     }
 
