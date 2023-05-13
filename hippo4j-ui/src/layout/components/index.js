006f56e822aaf7782636735521ce218c0f493489
chen.ma <machen@apache.org>
2023-05-13 11:21:34 +0800

    Refactor front-end project directory name and location (#1245)

--- hippo4j-ui/src/layout/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as AppMain } from './AppMain';
-export { default as Navbar } from './Navbar';
-export { default as Sidebar } from './Sidebar/index.vue';
---
+++ threadpool/console/src/views/profile/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="app-container">
+    <div v-if="user">
+      <el-row :gutter="20">
+
+        <el-col :span="6" :xs="24">
+          <user-card :user="user" />
+        </el-col>
+
+        <el-col :span="18" :xs="24">
+          <el-card>
+            <el-tabs v-model="activeTab">
+              <el-tab-pane label="Account" name="account">
+                <account :user="user" />
+              </el-tab-pane>
+            </el-tabs>
+          </el-card>
+        </el-col>
+
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Account from './components/Account.vue'
+
+export default {
+  name: 'Profile',
+  components: { Account },
+  data() {
+    return {
+      user: {},
+      activeTab: 'activity'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name',
+      'avatar',
+      'roles'
+    ])
+  },
+  created() {
+    this.getUser()
+  },
+  methods: {
+    getUser() {
+      this.user = {
+        name: this.name,
+        role: this.roles.join(' | '),
+        email: 'admin@test.com',
+        avatar: this.avatar
+      }
+    }
+  }
+}
+</script>
