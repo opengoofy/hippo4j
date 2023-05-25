57c388620ee6c908e2931743c981bdb95449b917
akib6074@gmail.com <akib.javed@apsissolutions.com>
2023-05-25 11:07:51 +0600

    resolved conflicts

--- hippo4j-common/src/test/java/cn/hippo4j/common/toolkit/JSONUtilTest.java
+++ hippo4j-common/src/test/java/cn/hippo4j/common/toolkit/JSONUtilTest.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
  package cn.hippo4j.common.toolkit;
 
  import com.fasterxml.jackson.core.type.TypeReference;
