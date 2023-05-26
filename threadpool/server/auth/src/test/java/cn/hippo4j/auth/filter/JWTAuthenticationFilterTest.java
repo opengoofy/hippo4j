2a2e281b86cc8d93d0a2c8744a674a0d68fe740f
annippolito <annibaleippolito@gmail.com>
2023-05-26 12:36:43 +0200

    increase codecov coverage by using junit instead of junit.jupiter

--- threadpool/server/auth/src/test/java/cn/hippo4j/auth/filter/JWTAuthenticationFilterTest.java
+++ threadpool/server/auth/src/test/java/cn/hippo4j/auth/filter/JWTAuthenticationFilterTest.java
@@ -18,15 +18,15 @@
 package cn.hippo4j.auth.filter;
 
 import cn.hippo4j.common.toolkit.ReflectUtil;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-class JWTAuthenticationFilterTest {
+public class JWTAuthenticationFilterTest {
 
     @Test
-    void getMessageTest() {
+    public void getMessageTest() {
         JWTAuthenticationFilter filter = new JWTAuthenticationFilter(null);
         Assertions.assertEquals("用户不存在", ReflectUtil.invoke(filter,
                 "getMessage", new UsernameNotFoundException("")));
