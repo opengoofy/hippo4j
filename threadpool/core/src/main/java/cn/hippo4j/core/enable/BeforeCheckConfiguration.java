86ec354959ce840a638dbd37c31d1e03e2301426
ayi <2084276225@qq.com>
2023-08-31 23:37:34 +0800

    feature: hippo4j config model in nacos config before check

--- threadpool/core/src/main/java/cn/hippo4j/core/enable/BeforeCheckConfiguration.java
+++ threadpool/core/src/main/java/cn/hippo4j/core/enable/BeforeCheckConfiguration.java
@@ -17,6 +17,7 @@
 
 package cn.hippo4j.core.enable;
 
+import cn.hippo4j.common.toolkit.MapUtil;
 import cn.hippo4j.common.toolkit.StringUtil;
 import cn.hippo4j.core.config.ConfigEmptyException;
 import cn.hippo4j.threadpool.dynamic.api.BootstrapPropertiesInterface;
@@ -26,6 +27,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -36,6 +38,7 @@ import java.util.Objects;
 public class BeforeCheckConfiguration {
 
     private final String bootstrapPropertiesClassName = "cn.hippo4j.springboot.starter.config.BootstrapProperties";
+    private final String bootstrapConfigPropertiesClassName = "cn.hippo4j.threadpool.dynamic.mode.config.properties.BootstrapConfigProperties";
 
     @Bean
     public BeforeCheckConfiguration.BeforeCheck dynamicThreadPoolBeforeCheckBean(@Autowired(required = false) BootstrapPropertiesInterface properties,
@@ -66,6 +69,24 @@ public class BeforeCheckConfiguration {
                 throw new ConfigEmptyException(
                         "Web server failed to start. The dynamic thread pool application name is empty.",
                         "Please check whether the [spring.application.name] configuration is empty or an empty string.");
+            }
+        }
+        checkFlag = properties != null && Objects.equals(bootstrapConfigPropertiesClassName, properties.getClass().getName()) && properties.getEnable();
+        if (checkFlag) {
+            Map<String, String> nacos = properties.getNacos();
+            if (MapUtil.isNotEmpty(nacos)) {
+                String group = nacos.get("group");
+                if (StringUtil.isBlank(group)) {
+                    throw new ConfigEmptyException(
+                            "Web server failed to start. The dynamic thread pool config nacos group is empty.",
+                            "Please check whether the [spring.dynamic.thread-pool.config-nacos-group] configuration is empty or an empty string.");
+                }
+                String dataId = nacos.get("data-id");
+                if (StringUtil.isBlank(dataId)) {
+                    throw new ConfigEmptyException(
+                            "Web server failed to start. The dynamic thread pool config nacos data-id is empty.",
+                            "Please check whether the [spring.dynamic.thread-pool.config-nacos-data-id] configuration is empty or an empty string.");
+                }
             }
         }
         return new BeforeCheckConfiguration.BeforeCheck();
