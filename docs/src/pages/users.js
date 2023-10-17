f036434a23ca8c790d12820c2fff39d0c0c82fba
youbo <1370246117@qq.com>
2023-10-17 23:45:45 +0800

    small fix

--- docs/src/pages/users.js
+++ docs/src/pages/users.js
@@ -12,22 +12,18 @@ function CompanyCards({ companies }) {
         .map((company) => (
           <div
             onClick={() => window.open(company.url)}
-            key={company.url}
-            // className="bg-white rounded-lg overflow-hidden shadow-sm transform cursor-pointer transition-all duration-500 hover:scale-110"
-            className="bg-white rounded-lg overflow-hidden shadow-sm transform cursor-pointer transition-all duration-500 hover:scale-110"
-            style={{ border: "1px solid #E5E7EB" }}
+            className="rounded-lg overflow-hidden  transform cursor-pointer transition-all duration-500 hover:scale-110"
+            // style={{ border: "1px solid #E5E7EB" }}
           >
-            <div className="flex dark:text-black items-center justify-center h-28">
-              <div
-                className="h-22 w-24 object-contain"
-                style={{ position: "relative" }}
-              >
-                <img
-                  src={useBaseUrl(company.logo)}
-                  alt={`${company.name}`}
-                  className="h-22 w-24 object-contain"
-                />
-              </div>
+            <div
+              className="h-20 logo-img-container p-1"
+              style={{ position: "relative" }}
+            >
+              <img
+                src={useBaseUrl(company.logo)}
+                alt={`${company.name}`}
+                className="max-h-[90%] max-w-[90%] h-auto object-contain"
+              />
             </div>
           </div>
         ))}
@@ -37,11 +33,11 @@ function CompanyCards({ companies }) {
 export default function OurUsers() {
   return (
     <Layout title="OurUsers" description="companies using our product">
-      <div className="max-w-screen-lg mx-auto dark:text-white">
+      <div className="w-[1200px] px-5 mx-auto dark:text-white">
         {/* text description*/}
-        <div className="max-w-screen-sm sm:mx-auto mx-10 text-center my-8 mt-16 ">
-          <div className="text-center mt-8">
-            <h2 className="text-3xl font-bold my-4">
+        <div className="max-w-screen-sm sm:mx-auto mx-10 text-center my-8 mt-14 mb-12 ">
+          <div className="text-center ">
+            <h2 className="text-3xl font-medium mb-7">
               <Translate
                 id="companyPage.title"
                 description="the title for the company page"
@@ -50,12 +46,12 @@ export default function OurUsers() {
               </Translate>
             </h2>
             <div
-              className="mx-auto h-1 bg-gradient-to-r from-green-400 to-blue-500 my-6"
+              className="mx-auto h-[0.215rem] bg-gradient-to-r from-green-400 to-blue-500 mb-6"
               style={{ width: "30%" }}
             ></div>
           </div>
-          <div className="flex flex-col items-center mx-auto">
-            <p className="text-lg leading-loose mb-2 max-w-1/2">
+          <div className="  mb-7 mx-auto">
+            <p className="text-lg leading-loose mb-2 ">
               <Translate
                 id="companyPage.descriptionText"
                 description="the description for the company page"
@@ -64,7 +60,7 @@ export default function OurUsers() {
                 This is our greatest motivation to move forward.
               </Translate>{" "}
             </p>
-            <p className="text-lg mb-4 max-w-1/2 leading-loose">
+            <p className="text-lg mb-5  leading-loose">
               <Translate
                 id="companyPage.questionText"
                 description="the text for the company question"
